eea3dfb769e78f1815b40335d398ce89343d75c4
akshaypasham <44039855+akshaypasham@users.noreply.github.com>
2021-01-21 15:25:08 -0800

    FIRST COMMIT

---
+++ gittest/src/gittest/test1.java
@@ -0,0 +1,28 @@
+
+package gittest;
+
+
+public class test1 {
+
+
+	public static void main(String[] args) {
+		int i,j,k ,l, n=10;
+		i=0 ;
+		j=1;
+		foo();
+		
+		
+		
+
+	
+	
+	
+
+	}
+
+	private static void foo() {
+		// TODO Auto-generated method stub
+		System.out.println("hii");
+	}
+
+}
